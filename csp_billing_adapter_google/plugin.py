95609530bb0033f88a14528617f90d6f86e87eed
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2023-05-25 11:21:11 +0100

    Get metadata from GCE to fill the customer_csp_data
    This references SUSE-Enceladus/csp-billing-adapter#20

--- csp_billing_adapter_google/plugin.py
+++ csp_billing_adapter_google/plugin.py
@@ -15,9 +15,13 @@
 #
 
 import csp_billing_adapter
+import urllib.request
+import urllib.error
 
 from csp_billing_adapter.config import Config
 
+METADATA_ADDR = 'http://169.254.169.254/computeMetadata/v1/'
+METADATA_HEADERS = {'Metadata-Flavor': 'Google'}
 
 @csp_billing_adapter.hookimpl
 def setup_adapter(config: Config):
@@ -41,4 +45,65 @@ def get_csp_name(config: Config):
 
 @csp_billing_adapter.hookimpl(trylast=True)
 def get_account_info(config: Config):
-    pass
+    """
+    Return a dictionary with account information
+
+    The information contains the cloud provider
+    and the metadata for instance and project.
+    """
+    account_info = _get_metadata()
+    account_info['cloud_provider'] = get_csp_name(config)
+
+def _get_metadata():
+    """
+    Return a dictionary with account information
+
+    The information contains the metadata for instance and project.
+    """
+    metadata = {}
+    metadata['instance'] = _get_instance_metadata()
+    metadata['project'] = _get_project_metadata()
+    return metadata
+
+def _get_instance_metadata():
+    """
+    Return a dictionary with instance metadata information
+
+    The information contains the metadata for name, license,
+    id, image and zone.
+    """
+    metadata_options = ['name', 'license', 'id', 'image', 'zone']
+    metadata = {}
+    for metadata_option in metadata_options:
+        uri = 'instance/'
+        if metadata_option not in ['license']:
+           uri += metadata_option
+        else:
+            if metadata_option in ['license']:
+                uri += 'licenses/0/id'
+
+        metadata[metadata_option] = _fetch_metadata(uri)
+    return metadata
+
+def _get_project_metadata():
+    """
+    Return a dictionary with project metadata information
+
+    The information contains the metadata for numeric project id and project-id.
+    """
+    metadata_options = ['numeric-project-id', 'project-id']
+    metadata = {}
+    for metadata_option in metadata_options:
+        metadata[metadata_option] = _fetch_metadata('project/' + metadata_option)
+    return metadata
+
+def _fetch_metadata(uri):
+    """Return the response of the metadata request."""
+    url = METADATA_ADDR + url
+    data_request = urllib.request.Request(url, headers=METADATA_HEADERS)
+    try:
+        value = urllib.request.urlopen(data_request).read()
+    except urllib.error.URLError:
+        return None
+
+    return value
